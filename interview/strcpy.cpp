95e0d2fcb5a99cc1bb996d6ab3d35831806393a5
bupt-wenxiaole <xiaolewen_bupt@163.com>
2016-04-03 17:23:58 +0800

    小修改

--- interview/strcpy.cpp
+++ interview/strcpy.cpp
@@ -7,7 +7,7 @@ char *strcpy_(char *dst,const char *src)
     assert(dst!=NULL && src!=NULL);
     
     char *dst_tmp=dst;
-    while((*dst++ = *src++)!='\0')
+    while((*dst++ = *src++)!='\0');
     *dst = '\0';               //这句必须要，否则无法判定字符串结束条件。  编译器告警，运行会出现不同的结果
     return dst_tmp;
     
