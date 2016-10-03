ccc348d22d9b69e993c95096a2641a6a1c7a1faf
xwen <xwen@vmware.com>
2016-10-03 00:03:04 -0700

    lc114.cpp

---
+++ lc114.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void flatten(TreeNode* root) {
+        if(root == NULL)
+            return;
+        while(root)
+        {
+            if(root -> left != NULL)
+            {
+                TreeNode* temp = root -> left;
+                while(temp -> right != NULL)
+                    temp = temp -> right;
+                temp -> right = root -> right;
+                root -> right = root -> left;
+                root -> left = NULL;
+            }
+            root = root -> right;
+        }
+        
+    }
+};
