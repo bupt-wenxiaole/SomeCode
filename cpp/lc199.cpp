1ea80e68a2215bc88dbfe4fdad5d490b67350276
bupt-wenxiaole <506539136@qq.com>
2016-10-07 14:29:33 +0800

    lc199v2.cpp

--- cpp/lc199.cpp
+++ cpp/lc199.cpp
@@ -9,51 +9,26 @@
  */
 class Solution {
 public:
-    vector<vector<int> > levelOrder(TreeNode* root)
-    {
-        int nextlevel = 0;
-        int tobeprinted = 1;
-        vector<int> curlevel;
-        vector<vector<int> > res;
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty())
-        {
-            curlevel.push_back(q.front() -> val);
-            if(q.front() -> left != NULL)
-            {
-                q.push(q.front() -> left);
-                nextlevel++;
-                
-            }
-            if(q.front() -> right != NULL)
-            {
-                q.push(q.front() -> right);
-                nextlevel++;
-            }
-            q.pop();
-            tobeprinted--;
-            if(tobeprinted == 0)
-            {
-                res.push_back(curlevel);
-                curlevel.clear();
-                tobeprinted = nextlevel;
-                nextlevel = 0;
-            }
-            
-        }
-        return res;
-        
-    }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> res;
-        vector<vector<int> > levelorder_res;
-        if(root == NULL)
-            return res;
-        levelorder_res = levelOrder(root);
-        for(auto t : levelorder_res)
-            res.push_back(t[t.size() - 1]);
+        int max_depth  = 0;
+        int cur_depth = 0;
+        vector<int> res; 
+        helper(root, cur_depth, max_depth, res);
         return res;
+    }
+    void helper(TreeNode* root, int& cur_depth, int& max_depth, vector<int>& res)
+    {
+        if(root == NULL)
+            return;
+        cur_depth++;
+        if(max_depth < cur_depth)
+        {
+            res.push_back(root -> val);
+            max_depth = cur_depth;
+        }
+        helper(root -> right, cur_depth, max_depth, res);
+        helper(root -> left, cur_depth, max_depth, res);
+        cur_depth--;
         
     }
 };
